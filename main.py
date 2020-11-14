011afc9bf4733642da2c0016e367defd807d79ec
SimplyProgger <clashofclansgamerarmoo@gmail.com>
2020-11-14 15:19:08 +0500

    added get weather function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from nltk.corpus import stopwords
 import nltk
+import commands
+from src.functions import web_api
 
 
 def clean_stop_words(sentence, language='russian'):
@@ -9,4 +11,15 @@ def clean_stop_words(sentence, language='russian'):
 	return without_stop_words
 
 
-print(clean_stop_words("Привет какая будет погода на завтра?"))
+def check_command(text):
+
+	string_text = ''
+
+	for i in text:
+		string_text += i + ' '
+
+	if string_text.lower().strip() in commands.commands["cmd"]["weather"]:
+		return web_api.get_weather()
+
+
+print(check_command(clean_stop_words("какая сейчас погода")))
